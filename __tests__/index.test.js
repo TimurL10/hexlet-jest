72a10ffc5e7cfc1a0a1332577e5373b51da776df
timurl <timurl@contourbi.ru>
2026-04-02 16:04:15 +0300

    test reverse text

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,19 @@
 import reverse from '../src/index.js'
 import { expect, test } from '@jest/globals'
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+test('reverse', () => { 
+  //expect(reverse('hello')).toEqual('olleh')
   expect(reverse('')).toEqual('')
+})
+
+test('test reverse text', () => {
+  let file_text = fs.readFileSync(path.join(__dirname,'..', '__fixtures__', 'text.txt'), 'utf-8');
+  fs.writeFileSync('reversed_text.txt', reverse(file_text));
+  expect(file_text).not.toEqual(reverse(file_text));
 })
